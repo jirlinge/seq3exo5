a5d39b14c7788e2ba3e7184f2a087444a264c46b
jirlinge <julienirlinger@gmail.com>
2021-02-16 12:48:34 +0100

    corrigé

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime thisDay = DateTime.Today;
+            Console.WriteLine("renseignez votre nom");
+            string name = Console.ReadLine();
+            Console.WriteLine("renseignez votre prénom");
+            string firstName = Console.ReadLine();
+            Console.WriteLine($"Bonjour {firstName} {name} nous somme le {thisDay:D} comment allez vous?");
+        }
+    }
+}
